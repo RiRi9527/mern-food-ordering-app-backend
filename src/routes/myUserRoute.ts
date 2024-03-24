26b059e8c03c483e4fc86c32360eddc48260ce2c
Hao Zhou <hzhou129527@gmail.com>
2024-03-24 19:42:22 -0400

    addding manage restaurant

--- src/routes/myUserRoute.ts
+++ src/routes/myUserRoute.ts
@@ -10,9 +10,9 @@ router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
 router.put(
   "/",
+  validateMyUserRequest,
   jwtCheck,
   jwtParse,
-  validateMyUserRequest,
   MyUserController.updateCurrentUser
 );
 
